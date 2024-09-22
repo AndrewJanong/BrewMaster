978c8cb99957a96eb6f62c483b319bd57760adeb
Andrew Janong <andrewjanong@gmail.com>
2024-09-23 02:27:31 +0800

    Create connection to test database

--- backend/connect_database.js
+++ backend/connect_database.js
@@ -3,12 +3,15 @@ const mysql = require("mysql2");
 // Load environment variables from .env file
 require('dotenv').config();
 
+// Check if the environment is set to 'test'
+const isTestEnvironment = process.env.NODE_ENV === 'test';
+
 // Create a database connection using environment variables
 const connection = mysql.createPool({
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    host: isTestEnvironment ? process.env.DB_HOST_TEST : process.env.DB_HOST,
+    database: isTestEnvironment ? process.env.DB_DATABASE_TEST : process.env.DB_DATABASE,
+    user: isTestEnvironment ? process.env.DB_USER_TEST : process.env.DB_USER,
+    password: isTestEnvironment ? process.env.DB_PASSWORD_TEST : process.env.DB_PASSWORD,
 });
 
 module.exports = connection;
